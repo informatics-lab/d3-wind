daee349ce5dd260aed642b34e753ad4b8fac8691
Thomas Powell <t.powell.meto@gmail.com>
2016-08-24 15:24:27 +0100

    improve nn vectorisation

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,13 @@ var uk = require('./data/uk.json');
 var moObSites = require('./data/mo-datapoint-obs-sites.json');
 
 
-const FRAME_RATE = 1000 / 2;
+const FRAME_RATE = 1000 / 1;
 const TRANSFORM_MAGNITUDE = 1 / 4;
+const AREA_OF_INFLUENCE = 100;
 const PARTICLE_AGE = 100;
 const PARTICLE_DECAY_RATE_PER_FRAME = 10;
-const NUM_SITES = 100;
 const NUM_PARTICLES = 2000;
+const MAX_SPEED = 20;
 var width;
 var height;
 const WIND_DIRECTIONS = d3.map()
@@ -37,7 +38,7 @@ var particles = [];
 init();
 
 function init() {
-    width = window.innerWidth;
+    width = window.innerWidth / 2;
     height = window.innerHeight;
 
     obsSites = moObSites.map(function (moObSite) {
@@ -50,15 +51,6 @@ function init() {
             properties: moObSite
         }
     });
-
-    // generate sites and particles
-    // obsSites = d3.range(NUM_SITES).map(function () {
-    //     return new ObsSite(new Vector2D(Math.random() * width, Math.random() * height),
-    //         Math.floor(Math.random() * 360),
-    //         Math.floor(Math.random() * 20));
-    // });
-
-
 
     particles = d3.range(NUM_PARTICLES).map(function () {
         var p = new Particle();
@@ -75,7 +67,7 @@ function init() {
         .center([0, 54.4])
         .rotate([4.4, 0])
         .parallels([50, 60])
-        .scale((width * height) / 200)
+        .scale((width * height) / 100)
         .translate([width / 2, height / 2]);
 
     var path = d3.geoPath()
@@ -96,7 +88,7 @@ function init() {
         .each(function(data){
             var centroid = path.centroid(data);
             data.properties.direction = Math.floor(Math.random() * 360);
-            data.properties.speed = Math.floor(Math.random() * 20);
+            data.properties.speed = Math.floor(Math.random() * MAX_SPEED);
 
             data.properties.position = new Vector2D(Math.floor(centroid[0]),Math.floor(centroid[1]));
             data.properties.transform = new Vector2D(data.properties.speed * Math.sin(data.properties.direction),
@@ -108,21 +100,35 @@ function init() {
 
     calculateTransforms();
 
-    // //draw the sites to screen
-    // svg.selectAll(".site")
-    //     .data(obsSites)
+    /* view calculated transforms */
+    // svg.selectAll(".transform-row")
+    //     .data(transforms)
     //     .enter()
-    //     .append("circle")
-    //     .attr("class", "site")
-    //     .attr("cx", function (d) {
-    //         return d.position.x;
+    //     .selectAll(".transform")
+    //     .data(function(d,transformRow) {
+    //         d.forEach(function (e){
+    //             e.transformRow=transformRow;
+    //         });
+    //         return d;
     //     })
-    //     .attr("cy", function (d) {
-    //         return d.position.y;
+    //     .enter()
+    //     .append("rect")
+    //     .attr("class", "transform")
+    //     .attr("x", function(d,x) {
+    //         return x;
     //     })
-    //     .attr("r", 10)
-    //     .on('mouseover', function(d) {
-    //        console.log(d);
+    //     .attr("y", function(d){
+    //         return d.transformRow;
+    //     })
+    //     .attr("width", 1)
+    //     .attr("height", 1)
+    //     .attr("opacity", 0.2)
+    //     .style("fill", function(d){
+    //         if(d && !(d.x === 0 && d.y === 0)) {
+    //             return "green"
+    //         } else {
+    //             return "red";
+    //         }
     //     });
 
     // draw the initial particles to screen
@@ -164,43 +170,26 @@ function animate() {
                 return d.position.y;
             })
             .attr("opacity", function (d) {
-                if (d.isInBounds() && d.age < 100) {
-                    return d.age / 100;
+                if (d.isInBounds()
+                    && d.age < PARTICLE_AGE - PARTICLE_DECAY_RATE_PER_FRAME
+                    && d.age > 0 + PARTICLE_DECAY_RATE_PER_FRAME) {
+                    return d.age / PARTICLE_AGE;
                 }
                 return 0;
+            })
+            .style("fill", function (d) {
+                return d3.interpolateViridis(d.speed/(MAX_SPEED/2));
+            })
+            .attr("r", function(d) {
+                return d.speed;
             });
 
     }, FRAME_RATE);
 }
 
 function calculateTransforms() {
+    var t1 = new Date().getTime();
     console.log("calculating pixel transforms...");
-
-    function getTransform(vector2D) {
-        var transform = {x: 0, y: 0};
-        var nns = [];
-        obsSites.forEach(function (obSite) {
-            var prob = obSite.properties.position.compare(vector2D, 300);
-
-            if (prob > 0) {
-                nns.push(new WeightedTransform(probToWeight(prob), obSite.properties.transform));
-            }
-        });
-        if (nns.length > 0) {
-            transform = nns.map(function (a) {
-                return {
-                    x: a.weight * a.transform.x,
-                    y: a.weight * a.transform.y
-                };
-            }).reduce(function (a, b) {
-                return {
-                    x: Math.floor(a.x + b.x),
-                    y: Math.floor(a.y + b.y)
-                };
-            });
-        }
-        return transform;
-    };
 
     for (var y = 0; y < height; y++) {
         for (var x = 0; x < width; x++) {
@@ -211,14 +200,62 @@ function calculateTransforms() {
             transforms[y][x] = getTransform(pixel);
         }
     }
-    console.log(transforms.length * transforms[0].length + ' pixel transforms calculated');
+    var t2 = new Date().getTime();
+    console.log(transforms.length * transforms[0].length + ' pixel transforms calculated in '+ (t2-t1));
 
 }
 
-function probToWeight(prob) {
-    var magicWeight = 0.21446;
-    return (prob - magicWeight) / (1 - magicWeight);
+function getTransform(vector2D) {
+    var transform = {x: 0, y: 0};
+    var sumComparisons = 0;
+    var nns = obsSites.filter(function (obSite) {
+        var dist = obSite.properties.position.distanceFrom(vector2D);
+        if (dist < AREA_OF_INFLUENCE) {
+            var comparison = 1 - normalise(0, AREA_OF_INFLUENCE, dist).toFixed(5);
+            if (comparison > 0) {
+                sumComparisons += parseFloat(comparison);
+                obSite.comparison = comparison;
+                return true;
+            }
+        }
+        return false;
+    });
+    if (nns.length > 0) {
+        transform = nns.map(function (a) {
+            var weight = a.comparison/sumComparisons;
+            return {
+                x: weight * a.properties.transform.x,
+                y: weight * a.properties.transform.y
+            };
+        }).reduce(function (a, b) {
+            return {
+                x: Math.floor(a.x + b.x),
+                y: Math.floor(a.y + b.y)
+            };
+        });
+    }
+    return transform;
 };
+
+
+/**
+ * Convert a value in a given range into a value between 1 and 0
+ * @param min - the minimum value of the current range
+ * @param max - the maximum value of the current range
+ * @param value - the value within the current range
+ * @returns {number} normalised valued
+ */
+function normalise(min, max, value) {
+    //Convert the range into a 0-1 range (float)
+    var a = value - min;
+    var b = max - min;
+    if (b == 0) {
+        return 1;
+    } else {
+        var c = a / b;
+        return c;
+    }
+}
 
 /*
  * HELPER CLASSES
@@ -227,31 +264,9 @@ function Vector2D(x, y) {
     var self = this;
     self.x = x;
     self.y = y;
-    self.compare = function (other, range) {
-        var dx = Math.abs(self.x - other.x);
-        var dy = Math.abs(self.y - other.y);
-        if (dx > range || dy > range) {
-            return 0;
-        } else {
-            var diff = Math.abs(dx - dy);
-            var distance = diff / range;
-            return 1 - distance;
-        }
+    self.distanceFrom = function(other) {
+        return Math.sqrt(Math.pow(other.x - self.x, 2) + Math.pow(other.y - self.y, 2)).toFixed(5);
     }
-}
-
-function ObsSite(position, direction, speed) {
-    var self = this;
-    self.position = position;
-    self.direction = direction;
-    self.speed = speed;
-    self.transform = new Vector2D(self.speed * Math.sin(self.direction), self.speed * Math.cos(self.direction));
-}
-
-function WeightedTransform(weight, transform) {
-    var self = this;
-    self.weight = weight;
-    self.transform = transform;
 }
 
 function Particle() {
@@ -259,24 +274,35 @@ function Particle() {
     self.previousPosition = null;
     self.position = null;
     self.age = null;
+    self.decayRate = null;
+    self.speed = null;
     self.spawn = function () {
         self.age = PARTICLE_AGE;
+        self.decayRate = Math.floor(Math.random() * (PARTICLE_DECAY_RATE_PER_FRAME - PARTICLE_DECAY_RATE_PER_FRAME/2)) + PARTICLE_DECAY_RATE_PER_FRAME/2;
         self.position = new Vector2D(Math.floor(Math.random() * width)
             , Math.floor(Math.random() * height));
         self.previousPosition = null;
     };
     self.step = function () {
-        self.age = self.age - Math.floor(Math.random() * PARTICLE_DECAY_RATE_PER_FRAME);
+        self.age = self.age - self.decayRate;
         if (self.age < 0) {
             self.spawn();
+            return;
         }
         if (self.isInBounds()) {
             self.previousPosition = self.position;
             var transform = transforms[self.previousPosition.y][self.previousPosition.x];
             self.position = new Vector2D(self.previousPosition.x + Math.floor(transform.x * TRANSFORM_MAGNITUDE),
                 self.previousPosition.y + Math.floor(transform.y * TRANSFORM_MAGNITUDE));
+            self.speed = self.previousPosition.distanceFrom(self.position);
+            if(self.speed == 0) {
+                self.age = -5;
+                return;
+            }
+            return;
         } else {
             self.age = -5;
+            return;
         }
     };
     self.isInBounds = function () {
